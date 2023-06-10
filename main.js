d7ce3752b0dea4642a5f9bf475bd5ae8f34b5359
SHAY7000 <seyiadeyekun@gmail.com>
2023-06-10 08:19:19 +0100

    complete array data and grid layout on css

--- main.js
+++ main.js
@@ -5,35 +5,48 @@ const meals = document.querySelector(".meal-container");
 
 // Creating arrays to store meal data
 const imgs = ["img1.jpeg", "img2.jpeg", "img3.jpeg", "img4.jpeg", "img5.jpeg", "img6.jpeg"];
+const imgText = ["Image of buttermilk Pancakes", "Image of diner Double", "Image of godzilla Milkshake", "Image of country Delight", "Image of egg Attack", "Image of oreo Dream"]
 const mealNames = ["Buttermilk Pancakes", "Diner Double", "Godzilla Milkshake", "Country Delight", "Egg Attack", "Oreo Dream"];
-const mealInfo = {"img1.jpeg": "$15.99",
+const mealInfo = {
+"img1.jpeg": "$15.99",
 "img2.jpeg": "$13.99",
 "img3.jpeg": "$6.99",
 "img4.jpeg": "$20.99",
 "img5.jpeg": "$22.99",
 "img6.jpeg": "$18.99"}
-//const imgText = {img1.jpg:img1, "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg"}
-const mealDesc =[" deliciously buttery and fluffy with golden, crisp edges and an irresistible buttermilk flavour",
-""];
+const mealDesc =[
+"Deliciously buttery and fluffy with golden, crisp edges and an irresistible buttermilk flavour",
+"Good old fashioned hamburger, no fancy toppings, no exotic meat or bun, just the best juicy burger you’ve been craving",
+"The premium milkshake that shakes your day off... Comes in various flavours of your choice",
+"Full English breakfasts contain: sausages, back bacon, eggs, tomatoes, mushrooms, fried bread, and beans",
+"Fried egg, thickly-cut bacon and cheddar cheese smothered over your famous single or double all-beef patties",
+"Blended Cream cheese and Hot Fudge Oreo in a milkshake, sounds like a dream"
+];
 
 // looping through imgs array and displaying images to webpage
 for(let i = 0; i < imgs.length; i++){
-const showImages = document.createElement("img");
+const infoSec = document.createElement("section")
+infoSec.setAttribute("class", "infoSection")
 const mealNameP = document.createElement("p")
 const priceDisplay = document.createElement("p")
 const showDescP = document.createElement("p")
+
+const showImages = document.createElement("img");
 showImages.setAttribute("src", "images/" + imgs[i])
+showImages.setAttribute("alt", `${imgText[i]}`)
 const price = mealInfo[imgs[i]]
 const mealName = mealNames[i]
 const showDesc = mealDesc[i]
 
-//showImages.setAttribute("alt", "imgs")
 priceDisplay.textContent = price
 mealNameP.textContent = mealName
 showDescP.textContent = showDesc
-meals.appendChild(showImages)
-meals.appendChild(priceDisplay)
-meals.appendChild(mealNameP)
-meals.appendChild(showDescP)
+
+infoSec.appendChild(showImages)
+infoSec.appendChild(mealNameP)
+infoSec.appendChild(priceDisplay)
+infoSec.appendChild(showDescP)
+
+meals.appendChild(infoSec)
 }
 
